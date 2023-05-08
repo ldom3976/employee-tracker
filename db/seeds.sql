9bb3273c950b3586c7d214e640ac12509a8e0a5b
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-08 16:47:16 -0400

    started seeds

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES  ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
+
+NSERT INTO role (title, salary, department_id)
+VALUES  ("");
