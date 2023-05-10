c096195de1312eab77dfbcc29fe5fc6e2b0dd352
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-10 10:59:14 -0400

    fixed typo

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ VALUES  ("Sales"),
         ("Finance"),
         ("Legal");
 
-NSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES  ("Sales Lead", 100000, 1),
         ("Salesperson", 80000, 1),
         ("Lead Engineer", 150000, 2),
