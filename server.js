db35748587d428ac6b559b2c17461c58083204cd
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-10 10:15:49 -0400

    added db connection to server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-// Dependencies
 const inquirer = require("inquirer");
+
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "soph123",
+        database: "employees_db",
+    },
+    console.log('connected')
+);
+
