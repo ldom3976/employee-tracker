6407a775736a6d4ff2d5bc9d89f27d7ea8c2a454
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-10 10:19:05 -0400

    added first question function to server

--- server.js
+++ server.js
@@ -13,3 +13,52 @@ const db = mysql.createConnection(
     console.log('connected')
 );
 
+function firstQuestion() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'intro',
+            message: 'What would you like to do?',
+            choices:[
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department',
+                'Quit'
+            ]
+        }
+    ]).then((answer) => {
+        switch(answer.intro) {
+            case 'View All Employees':
+                viewEmployees();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                updateRole();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'View All Departments':
+                viewDepartments();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Quit':
+                console.log('Goodbye!');
+                db.end();
+                break;
+            }
+        });
+    };
+
+
