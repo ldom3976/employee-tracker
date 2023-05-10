eedb965a2e27afe46156090d77a7b5fb5f7c8127
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-10 11:28:38 -0400

    fixed connection error

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
+
 const db = mysql.createConnection(
     {
-        host: "localhost",
+        host: "127.0.0.1",
+        port: 3306,
         user: "root",
         password: "soph123",
         database: "employees_db",
